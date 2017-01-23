4403cd1a100e3e4cec19273ee70be20230053a7d
chester.reyes <chester.reyes@hautelook.com>
2017-01-23 11:39:41 -0800

    Performed Clean-up.

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -11,17 +11,9 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.ReadKey();
             Console.WriteLine("Hello World 2!!!");
             Console.WriteLine("Hello World 3!!!");
-
-            Console.WriteLine("Hello World 4!!!");
-
-            Console.WriteLine("Hello World 5!!!");
-
-            Console.WriteLine("Hello World 6!!!");
-            
-                    Console.WriteLine("Hello World 7!!!");
+            Console.ReadKey();
         }
     }
 }
