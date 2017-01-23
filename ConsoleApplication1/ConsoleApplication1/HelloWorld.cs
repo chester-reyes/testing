cddb2e600c7e757d24c98a3bf4bf57b74a96732b
chester.reyes <chester.reyes@hautelook.com>
2017-01-23 14:52:25 -0800

    Initial Submission of Hello World With IOC.

---
+++ ConsoleApplication1/ConsoleApplication1/HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    public class HelloWorld : IHelloWorld
+    {
+        public void DisplayMessage()
+        {
+            Console.WriteLine("Hello World!!!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/IOC.cs
@@ -0,0 +1,13 @@
+﻿using HauteLook.Shared.IOC.Types;
+
+namespace ConsoleApplication1
+{
+    public static class IoC
+    {
+        public static void Register(IContainer container)
+        {
+            container.For<IContainer>().Use(container);
+            container.For<IHelloWorld>().Use<HelloWorld>();
+        }
+    }
+}
