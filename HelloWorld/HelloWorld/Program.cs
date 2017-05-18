4e1623e2365d4b75f967ba4d7d9c1752a68d2a47
chester-reyes <chester.reyes@hautelook.com>
2017-05-18 14:45:40 -0700

    Update Program.cs

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -11,8 +11,6 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World 2!!!");
-            Console.WriteLine("Hello World 3!!!");
             Console.ReadKey();
         }
     }
